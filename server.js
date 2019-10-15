f32094257189f2dafce3f4c4c43eda84054b64ef
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-15 11:48:17 -0700

    added routes

---
+++ server.js
@@ -0,0 +1,48 @@
+//---------------------- SETUP ------------------------------//
+const express = require('express');
+const bodyparser = require('body-parser');
+
+// Server App 
+const app = express();
+
+
+// Server PORT
+const PORT = process.env.PORT || 3000;
+
+//------------------------- MiddleWare --------------------------------------//
+
+//Server Static  Asset to 
+app.use(express.static(`${__dirname}/public`));
+
+//------------------------------------ app routes------------------------------//
+//GET Home route(root)
+app.get('/', (req, res) => {
+  res.sendFile('views/index.html', {
+    root: __dirname, 
+  });
+});
+
+// events
+app.get('/events', (req, res) => {
+  res.sendFile('views/events.html', {
+    root: __dirname, 
+  });
+});
+
+// P Gallery
+app.get('/photo-gallery', (req, res) => {
+  res.sendFile('views/photo-gallery.html', {
+    root: __dirname, 
+  });
+});
+
+// V  Gallery 
+app.get('/video-gallery', (req, res) => {
+  res.sendFile('views/v-gallery.html', {
+    root: __dirname, 
+  });
+});
+
+
+//------------------------------------  Start server ----------------------------//
+app.listen(PORT, () => console.log(`Server started on http://localhost:${PORT}`));
