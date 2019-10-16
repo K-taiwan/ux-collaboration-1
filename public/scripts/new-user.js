246c29b17d3e53a21b8fdddb0c0eb7a1b0316dbf
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-16 12:55:57 -0700

    popop is workinggit add -Agit add -Agit add -A

--- public/scripts/new-user.js
+++ public/scripts/new-user.js
@@ -1,7 +1,13 @@
 // ---------------------- Global Vars ---------------------------------//
 const $newSubscriber = $('#newSubscriber'); 
 let subscriberInterests = [];
+const modal = document.getElementById("myModal");
 
+// Get the button that opens the modal
+const btn = document.getElementById("subuser");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
 
 
 // ----------- functions -------------- //
@@ -22,30 +28,6 @@ const handleSubmitForm = (event) => {
 
   validateInterests();
   formData();
-  var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("subuser");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
   $('.empty').val('');
   $('.checkbox').prop('checked', false);
   subscriberInterests = [];
@@ -53,7 +35,20 @@ window.onclick = function(event) {
 }
 
 const onSuccess = () => {
+
+    modal.style.display = "block";
   
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 }
 
 const onError = () => {
