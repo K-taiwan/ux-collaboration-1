b88dd0209184cbfec7a542c738b8ab6c7b196f79
Adonis Martin quiñones <adonisdesuvikdo@gmail.com>
2019-10-16 12:25:19 -0700

    popop needs to be fix

--- public/scripts/new-user.js
+++ public/scripts/new-user.js
@@ -22,6 +22,30 @@ const handleSubmitForm = (event) => {
 
   validateInterests();
   formData();
+  var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("subuser");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
   $('.empty').val('');
   $('.checkbox').prop('checked', false);
   subscriberInterests = [];
@@ -29,7 +53,6 @@ const handleSubmitForm = (event) => {
 }
 
 const onSuccess = () => {
-  console.log('success!');
   
 }
 
@@ -73,6 +96,12 @@ const validateInterests = () => {
   }
   console.log(subscriberInterests);
 }
+
+// ------------------------------------ popup ------------------------//
+// Get the modal
+
+
+
 
 // ------------------------ empty ---------------------------//
 $('.empty').empty();
